92dd47069319d03c4323a2f64256480dd9a2164f
FMR7 <14892931+FMR7@users.noreply.github.com>
2024-12-09 00:05:34 +0100

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Factorio Stackable Asteroids
+Adds stacking to all types of asteroids.
+
+# Mod Page
+https://mods.factorio.com/mod/stackable-asteroids
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({{
+    type = "int-setting",
+    name = "asteroid-stack-size",
+    localised_name = "Asteroid Stack Size",
+    localised_description = "To get the base stack size use 1",
+    setting_type = "startup",
+    minimum_value = 1,
+    default_value = 50
+}})
